8f67e8f7de7cff11ff51e8f67439a42b5a36b2ef
Madicken Eriksson <conlerc@live.se>
2022-10-22 15:29:03 +0200

    mainmenu done

--- src/components/MenuIcon.js
+++ src/components/MenuIcon.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const MenuIcon = ({link, icon, quantity }) => {
+const MenuIcon = ({link, icon, quantity, hideMobil, hideTablet }) => {
   return (
-    <NavLink className="menu-icon" to={link} end>
+    <NavLink className={`menu-icon ${hideMobil ? "hideMobil" : ""} ${hideTablet ? "hideTablet" : ""}`} to={link} end>
         <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-theme">{quantity}</span>
         <i className={icon}></i>
     </NavLink>
